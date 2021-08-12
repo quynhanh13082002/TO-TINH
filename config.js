3ee522e88ed4a3e8f034cc8c85fe7f6af4247eb3
quynhanh13082002 <88850117+quynhanh13082002@users.noreply.github.com>
2021-08-13 05:19:36 +0700

    Update config.js

--- config.js
+++ config.js
@@ -1,19 +1,16 @@
 const CONFIG = {
-    introTitle: 'Tên người yêu bạn à!',
-    introDesc: `Trái đất vốn lạ thường
-    Mà sao em cứ đi nhầm đường
-    Lạc vào tim anh lẻ loi
-    Đằng sau chữ yêu đây là thương`,
-    btnIntro: 'hihi',
-    title: 'Phải chăng em đã yêu ngay từ cái nhìn đầu tiên 😙',
+    introTitle: '13/08/2002',
+    introDesc: `Mùa covid rảnh rổi làm cái nì chúc mừng sinh nhật bà nì..hehe`,
+    btnIntro: 'bấm vào đây còn nữa',
+    title: '19 tuổi rồi , chúc mọi thứ tốt đẹp nah ♥♥♥♥♥',
     desc: 'Phải chăng em đã say ngay từ lúc thấy nụ cười ấy ',
     btnYes: 'Vẫn cứ là thích anh <33',
     btnNo: 'Không, Anh trai à :3',
     question:'Trên thế giới hơn 7 tỉ người mà sao em lại yêu anh <3',
     btnReply: 'Gửi cho anh <3',
     reply: 'Yêu thì yêu mà không yêu thì yêu <33333333',
-    mess: 'Anh biết mà 🥰. Yêu em nhiều nhiều 😘😘',
-    messDesc: 'Tối nay 7h anh qua đón nhé công chúa.',
-    btnAccept: 'Okiiiii lun <3',
+    mess: 'Anh biết mà 🥰.Haha...nói đùa chớ SINH NHẬT VUI VẺ..KKKK😘😘',
+    messDesc: 'Kí tên A Huy đẹp đẹp..hehe',
+    btnAccept: 'hết rồi nì..baiibaii <3',
     messLink: 'http://fb.com' //link mess của các bạn. VD: https://m.me/nam.nodemy
 }
