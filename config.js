b8b02b7cab83d2328b2754443cceffae8f500357
quynhanh13082002 <88850117+quynhanh13082002@users.noreply.github.com>
2021-08-13 05:48:17 +0700

    Update config.js

--- config.js
+++ config.js
@@ -1,16 +1,16 @@
 const CONFIG = {
     introTitle: '13/08/2002',
-    introDesc: `Mùa covid rảnh rổi làm cái nì chúc mừng sinh nhật bà nì..hehe`,
+    introDesc: `Mùa covid rảnh rổi làm cái nì chúc mừng sinh nhật bà..hehe...làm cho vui thôi, hong quạu`,
     btnIntro: 'bấm vào đây còn nữa',
-    title: '19 tuổi rồi , chúc mọi thứ tốt đẹp nah ♥♥♥♥♥',
+    title: '19 tuổi rồi , chúc mọi thứ tốt đẹp nah ♥♥♥♥♥Xinh đẹp, thành công, hạnh phúc',
     desc: 'Phải chăng em đã say ngay từ lúc thấy nụ cười ấy ',
     btnYes: 'Vẫn cứ là thích anh <33',
     btnNo: 'Không, Anh trai à :3',
     question:'Trên thế giới hơn 7 tỉ người mà sao em lại yêu anh <3',
     btnReply: 'Gửi cho anh <3',
-    reply: 'Yêu thì yêu mà không yêu thì yêu <33333333',
-    mess: 'Anh biết mà 🥰...nói vui chớ SINH NHẬT VUI VẺ..chúc tất cả😘😘',
-    messDesc: 'Kí tên A Huy đẹp ',
+    reply: 'Yêu thì yêu mà không yêu cũng iuuu <33333333',
+    mess: 'tui biết mà 🥰...nói vui chớ SINH NHẬT VUI VẺ..chúc tất cả. Mai mốt hết dịch nhớ đãi tui ăn 1 bữa ra trò là được rồi nì..hehe😘😘',
+    messDesc: 'Kí tên: A Huy đẹp ',
     btnAccept: 'baiibaii <3',
  
 }
